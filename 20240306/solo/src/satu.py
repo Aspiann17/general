98d59ade347cf8cf16044b61b5e4ff1d22b7fa55
Aspian <p.aspian1738@gmail.com>
2024-03-14 02:16:12 +0800

    Orang macam apa yang ngoding di jam 2 pagi

--- 20240306/solo/src/satu.py
+++ 20240306/solo/src/satu.py
@@ -2,33 +2,37 @@ from util import intput
 
 def main():
     if debug:
-        tinggi, berat = 200,50 # 12.5
+        tinggi, berat = 150, 60 # 26.6
     else:
         tinggi = intput("Masukkan tinggi badan (cm): ")
         berat  = intput("Masukkan berat badan (kg): ")
 
-    print("Kondisi fisik: ", end="")
-
     bmi = berat / ((tinggi / 100) ** 2)
 
-    if bmi > 0 and bmi < 18.5:
+    if debug:
+        print(f"Tinggi: {tinggi}\nBerat: {berat}\nBMI : {bmi}")
+
+    print("Fisik: ", end="")
+
+    if bmi < 18.5:
         print("Kurus")
     elif bmi >= 18.5 and bmi <= 24.9:
         print("Ideal")
-    elif bmi > 25:
+    elif bmi >= 25 and bmi <= 29.9:
         print("Gemuk")
+    elif bmi >= 30:
+        print("Obesitas")
+    elif bmi < 0:
+        print("Alien")
     else:
-        print("Tidak normal")
-
-    if debug:
-        print(f"BMI: {bmi}")
+        print("Tidak manuk")
 
 if __name__ == "__main__":
     debug = False
-
-    # Belum di uji
     main()
 
-
-
-# https://www.rsmurniteguh.com/id/tools/kalkulator_bmi
+# Source: https://www.siloamhospitals.com/informasi-siloam/artikel/cara-menghitung-bmi
+# Kurang dari 18,5 berarti berat badan kurang (underweight).
+# Antara 18,5 - 24,9 berarti berat badan normal
+# Antara 25-29,9 berarti berat badan berlebih (overweight).
+# Di atas 30 berarti obesitas

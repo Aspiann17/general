34cad19d3adec7acdda2552e6f7c8369d70a0f06
Aspian <p.aspian1738@gmail.com>
2024-05-06 19:32:16 +0800

    Menambah fitur hehe

--- php/crud/users/index.php
+++ php/crud/users/index.php
@@ -9,18 +9,18 @@
     <link rel="stylesheet" href="style.css">
 </head>
 
-<body>
+<body <?=(Utils::isset("hehe", "true") ? "class='hehe'" : null)?>>
     <main>
         <div class="top"><h1>Users</h1></div>
-            <form action="" method="post">
-                <div class="input">
-                    <input type="text" name="username" placeholder="Username" autocomplete="off" required>
-                    <input type="password" name="password" placeholder="Password" required>
-                </div>
-
-                <div class="button">
-                    <button name="action" value="login"><h3>Login</h3></button>
-                    <button name="action" value="register"><h3>Register</h3></button>
+        <form action="" method="post">
+            <div class="input">
+                <input type="text" name="username" placeholder="Username" autocomplete="off" required>
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+            
+            <div class="button">
+                <button name="action" value="login"><h3>Login</h3></button>
+                <button name="action" value="register"><h3>Register</h3></button>
             </form>
         </div>
     </main>

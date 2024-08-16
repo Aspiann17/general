6ab2f259f91c14464b5ba7011c7080b980a6a1ba
Aspian <p.aspian1738@gmail.com>
2024-08-16 23:23:00 +0800

    android: intent

--- sch/xi/Android/001/README.md
+++ sch/xi/Android/001/README.md
@@ -3,3 +3,8 @@
 ## Layout
 - https://stackoverflow.com/questions/45776994/android-gridlayout-columnspan-not-spanning
 - https://stackoverflow.com/questions/2710793/what-is-the-equivalent-of-colspan-in-an-android-tablelayout # no
+
+## Intent
+- https://www.geeksforgeeks.org/how-to-make-a-phone-call-from-an-android-application/
+- https://stackoverflow.com/questions/68808796/how-can-you-launch-termux-through-another-android-app
+- https://stackoverflow.com/questions/3872063/how-to-launch-an-activity-from-another-application-in-android
--- sch/xi/Android/001/app/src/main/java/id/my/aspian/latihan001/MainActivity.java
+++ sch/xi/Android/001/app/src/main/java/id/my/aspian/latihan001/MainActivity.java
@@ -8,6 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+// Intent
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.net.Uri;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +26,75 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        // Github Intent
+        findViewById(R.id.btn_github).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Igithub = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Aspiann17"));
+                startActivity(Igithub);
+            }
+        });
+
+        // Youtube Intent
+        findViewById(R.id.btn_youtube).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Iyoutube = new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtube.com/"));
+                startActivity(Iyoutube);
+            }
+        });
+
+        // Shopee Intent
+        findViewById(R.id.btn_shopee).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Ishopee = new Intent(Intent.ACTION_VIEW, Uri.parse("https://shopee.co.id/"));
+                startActivity(Ishopee);
+            }
+        });
+
+        // Telepon Intent
+        findViewById(R.id.btn_call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone_number = String.format("tel:%s", R.string.phone_number);
+                Intent Icall = new Intent(Intent.ACTION_CALL, Uri.parse(phone_number));
+                startActivity(Icall);
+            }
+        });
+
+        // Termux Intent
+        findViewById(R.id.btn_termux).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Itermux = getPackageManager().getLaunchIntentForPackage("com.termux");
+                if (Itermux != null) {
+                    startActivity(Itermux);
+                };
+            }
+        });
+
+        // Nix on Droid Intent
+        findViewById(R.id.btn_nod).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Inod = getPackageManager().getLaunchIntentForPackage("com.termux.nix");
+                if (Inod != null) {
+                    startActivity(Inod);
+                };
+            }
+        });
+
+        // Facebook Intent
+        findViewById(R.id.btn_facebook).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Ifacebook = new Intent(Intent.ACTION_VIEW, Uri.parse("https://facebook.com"));
+                startActivity(Ifacebook);
+            }
         });
     }
 }

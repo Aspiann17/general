fcb11c8b28c44110ad7985b7787ef3bbfe03a40a
Aspian <p.aspian1738@gmail.com>
2024-10-17 22:28:44 +0800

    web: 003

---
+++ sch/xi/Web/003/core/utils.php
@@ -0,0 +1,41 @@
+<?php
+
+function template(string $template, array $variables = []) : string {
+
+    extract($variables);
+
+    ob_start();
+
+    require __DIR__ . "/../assets/templates/$template.php";
+
+    return ob_get_clean();
+}
+
+function is_set($key, $value) : bool {
+
+    if (isset($_GET[$key]) && $_GET[$key] === $value) return true;
+
+    elseif (isset($_POST[$key]) && $_POST[$key] === $value) return true;
+
+    elseif (isset($_SESSION[$key]) && $_SESSION[$key] === $value) return true;
+
+    return false;
+}
+
+function alert(string $message) : void {
+    echo "<script>alert('$message')</script>";
+}
+
+function start() : void {
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+}
+
+function check() : void {
+    start();
+
+    if (!is_set("login", true)) {
+        header("location: login.php");
+    }
+}

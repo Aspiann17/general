f8ddf17af6cbc95b4f5e30b628f1be24e94f7b3d
Aspian <p.aspian1738@gmail.com>
2024-10-20 12:29:27 +0800

    android 007: something

---
+++ sch/xi/Android/007/README.md
@@ -0,0 +1,6 @@
+# Link
+- [MainActivity](https://github.com/Aspiann17/code/blob/main/sch/xi/Android/007/app/src/main/java/id/my/aspian/t007/MainActivity.java)
+- [activity_main](https://github.com/Aspiann17/code/blob/main/sch/xi/Android/007/app/src/main/res/layout/activity_main.xml)
+
+# Note
+Sebelum melakukan compile, pastikan file gambar img_{1, 2, 3} ada pada drawable.
--- sch/xi/Android/007/app/src/main/java/id/my/aspian/t007/MainActivity.java
+++ sch/xi/Android/007/app/src/main/java/id/my/aspian/t007/MainActivity.java
@@ -2,10 +2,12 @@ package id.my.aspian.t007;
 
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,10 +26,17 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        Window window = getWindow();
+        window.setNavigationBarColor(ContextCompat.getColor(this, R.color.primary));
+//        window.setStatusBarColor(ContextCompat.getColor(this, R.color.primary));
+
         ImageView picture = findViewById(R.id.picture);
         int[] drawables = {R.drawable.tr_1, R.drawable.tr_2, R.drawable.tr_3};
 
+        // Indeks akan bertambah satu setiap kali tombol ditekan dan
+        // akan direset menjadi '0' jika mimiliki nilai sama dengan panjang array.
         findViewById(R.id.change).setOnClickListener(v -> {
+
             picture.setImageResource(drawables[index]);
             ((TransitionDrawable) picture.getDrawable()).startTransition(500);
 

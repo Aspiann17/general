dcb8a7311b3dd494ee70fd75e14b4a2daa89d109
Aspian <p.aspian1738@gmail.com>
2024-11-29 11:01:53 +0800

    web 007-gt-2: delete

--- sch/xi/Web/007-dt-2/resources/views/posts/show.blade.php
+++ sch/xi/Web/007-dt-2/resources/views/posts/show.blade.php
@@ -31,8 +31,10 @@
                     class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Back</button>
             </a>
 
-            <button id="delete-button" type="submit"
-                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+            <button id="delete-button" type="button"
+                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                Delete
+            </button>
 
             <button type="submit"
                 class="text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -41,6 +43,14 @@
 
         </form>
     </main>
+
+    {{-- Delete Form --}}
+    <form id="delete-form" action="/posts/{{ $post->id }}" method="POST">
+        @csrf
+        @method('DELETE')
+
+        <input type="hidden" name="id" value="{{ $post->id }}">
+    </form>
 
     @if ($errors->any())
         @foreach ($errors->all() as $error)
